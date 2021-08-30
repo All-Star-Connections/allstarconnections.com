694a91b5daff1ed462fd198231e8308d6e86fca5
John Grattan <contact@johngrattan.com>
2021-08-30 17:38:43 -0400

    Added favicon

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/graphics/all-star-connections-job-recruiting-agency-favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
